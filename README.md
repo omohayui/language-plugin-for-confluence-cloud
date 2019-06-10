0c95a3d5040d9e25e58bdd089b5c90d42df70e4b
Yui Takeuchi <omohayui@gmail.com>
2019-06-10 19:52:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Language plugin for confluence cloud
+Visitors can switch languages and browse content in their language.  
+This plugin works at local and Heroku.
+
+## Supported Languages
+* ja
+* en
+* zh
+* ko
+
+## Atlassian Connect App using Express
+
+[Read the docs](https://bitbucket.org/atlassian/atlassian-connect-express/src/master/README.md).
---
+++ routes/index.js
@@ -0,0 +1,77 @@
+export default function routes(app, addon) {
+    // Redirect root path to /atlassian-connect.json,
+    // which will be served by atlassian-connect-express.
+    app.get('/', (req, res) => {
+        res.redirect('/atlassian-connect.json');
+    });
+
+    // Verify that the incoming request is authenticated with Atlassian Connect.
+    app.get('/v1/dispatchLanguage', addon.authenticate(), function(req, res){
+
+        //  Grab all input parameters - sent through to us as query params.
+        var language    = req.query['language'],
+            pageTitle   = req.query['pageTitle'],
+            pageId      = req.query['pageId'],
+            pageVersion = req.query['pageVersion'],
+            macroId     = req.query['macroId'];
+        var clientKey = req.context.clientKey;
+
+        // Default Language: ja
+        var selected = 'ja';
+        if (req.cookies && req.cookies.docslang) {
+            selected = req.cookies.docslang;
+        } else if (req.headers['accept-language']) {
+//            selected = req.headers['accept-language']; // TODO convert en-US -> en
+        }
+        console.log('selected: ' + selected + ', language: ' + language);
+
+        // Selected Language?
+        if (language != selected) {
+            res.send();
+            return;
+        }
+
+        //  Execute API request to get the macro body.
+        getHTTPClient(clientKey).get(
+            '/rest/api/content/' + pageId +
+            '/history/' + pageVersion +
+            '/macro/id/' + macroId,
+            function(err, response, contents){
+
+                //  If we've encountered errors, render an error screen.
+                if(err || (response.statusCode < 200 || response.statusCode > 299)) {
+                    console.log(err);
+                    res.render('<strong>An error has occurred :( '+ response.statusCode +'</strong>');
+                }
+
+                //  Parse the response, and send the body through.
+                contents = JSON.parse(contents);
+
+                //  Render with required body.
+                res.render('language', { body : contents.body, pageTitle: pageTitle});
+            }
+        );
+    });
+
+    app.get('/setLanguage', addon.authenticate(), function(req, res){
+        var lang = req.query['lang'];
+        if (lang) {
+            res.setHeader('Set-Cookie', ['docslang=' + lang]);
+            res.send('set ' + lang);
+            return;
+        }
+        res.send('not set');
+    });
+
+    // Returns a HTTP client which can make calls to our host product.
+    // @param clientKey formed when app created.
+    // @param userKey formed when app created.
+    // @returns {*} http client
+    function getHTTPClient (clientKey, userKey){
+        return addon.httpClient({
+            clientKey : clientKey,
+            userKey   : userKey,
+            appKey    : addon.key
+        });
+    }
+}
